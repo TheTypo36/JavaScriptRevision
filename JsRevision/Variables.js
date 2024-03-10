b367b2c06e425b471264e6dbc176d6839cbd2848
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-10 15:42:28 +0000

    first commit

---
+++ JsRevision/Variables.js
@@ -0,0 +1,6 @@
+const name = "ashish";
+let accountEmail ="ashishaabro12@gmail.com"
+var accountPassword = "121212"
+accountCity = "Jaipur";
+
+console.log(name,accountEmail,accountPassword,accountCity); 
