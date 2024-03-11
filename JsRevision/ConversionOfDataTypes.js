823b3f7dd9c78918842690590d145fc6926af382
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-11 03:49:49 +0000

    conversion dataType:

---
+++ JsRevision/ConversionOfDataTypes.js
@@ -0,0 +1,28 @@
+let score = "33";
+
+let scoreInNumber = Number(score);
+
+console.log(typeof score);
+console.log(typeof scoreInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1, false => 0
+//undefined => NaN
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+
+// "" => false,  "ashish" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
