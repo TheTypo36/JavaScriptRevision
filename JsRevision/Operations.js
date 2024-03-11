db786e46eaf82e18163b1514e3c71844003bdcfe
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-11 05:39:59 +0000

    dataType

---
+++ JsRevision/Operations.js
@@ -0,0 +1,17 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+console.log(3**2);
+console.log(5%6);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+console.log(str1+str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+
+// str + interger => string 
+console.log(true); 
--- JsRevision/dataType.js
+++ JsRevision/dataType.js
@@ -7,6 +7,8 @@ let age = 23;
 let isLoggedIn = true;
 
 
+//*******Primitive dataType:(7 types)*********
+
 //number
 //char
 //string
@@ -15,8 +17,49 @@ let isLoggedIn = true;
 //bigInt 
 // null => standalone value
 //undefined
+//Symbol
 
-//object
+
 
 console.log(typeof name);
-  
+
+//symbols
+
+const id = Symbol('123');
+
+const anotherId = Symbol('123');
+
+console.log(id===anotherId);
+
+const bigNumber = 11021234123412393434;
+
+
+console.log(bigNumber);
+
+//*********Non-primitive dataType(Reference type)***********
+
+//object
+//Array
+//function 
+
+
+//javaScript is a dynamically typed language,which means that datatypes of variables are 
+//determined by the value they hold at runtime.
+
+const heros = ["shaktiman","naagraj","doga"];
+
+const student1 = {
+    name: "Ashish",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("helloWorld");
+}
+
+console.log(typeof bigNumber)
+console.log(typeof heros)
+console.log(typeof student1)
+console.log(typeof myFunction)
+console.log(typeof id);
