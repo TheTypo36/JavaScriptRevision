449357d560bb0f9e1b2aab38f947540091f451d3
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-12 15:35:24 +0000

    :stack and heap

---
+++ JsRevision/StackAndHeap.js
@@ -0,0 +1,22 @@
+let UserOne = "ashish"
+let userTwo = UserOne;
+
+userTwo = "anand";
+
+
+console.log(UserOne);
+console.log(userTwo);
+
+let data1 = {
+    email: "ashish@gmail.com",
+    password: "1234"
+}
+
+let data2 = data1;
+
+data2.email = "anand@google.com",
+data2.password = "334"
+
+console.log(data1);
+console.log(data2);
+
