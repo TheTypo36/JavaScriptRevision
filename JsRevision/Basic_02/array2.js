6f5099fe0deb3bf3fdbb491b60356d5f5b2b3b99
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-14 07:03:50 +0000

    array:part2

---
+++ JsRevision/Basic_02/array2.js
@@ -0,0 +1,28 @@
+ let marvel_heros = ["Ironman","spiderman","thor","captainAmerica"];
+ let dc_heros = ["superman","batman","Flash"];
+
+
+//concat is used for combining error ,but it is not the modern way
+let combines_heros = marvel_heros.concat(dc_heros);
+console.log(marvel_heros);
+console.log(combines_heros);
+
+//using spread operator to combine is better way 
+let combines_heros2 = [...dc_heros,...marvel_heros];
+console.log(combines_heros2);
+
+let another_arr =  [1,2,3,[4,5,6],8,9,[10,11,12,[13,14,15]]];
+console.log(another_arr);
+
+console.log(another_arr.flat(Infinity));
+
+console.log(Array.from("Ashish"));
+const students ={
+    1: "ashish",
+    2: "anand",
+    3: "Anvi",
+    4: "Sanjay",
+    5: "pinki"
+}
+
+console.log(Object.values(students));
