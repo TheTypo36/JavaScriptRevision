0d7d1c99a8833b16e555cb7633f82986fd044041
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-18 16:25:40 +0000

    arrow function usage

--- JsRevision/Basic_03/arrow.js
+++ JsRevision/Basic_03/arrow.js
@@ -22,3 +22,37 @@ user.welcomeMessage();
     console.log(this.username);
  }
  chai();
+
+ const chai2 = function(){
+    let username = "anand";
+    console.log(this.username);
+}
+chai2();
+
+const chai3 = () =>{
+    let username = "anvi";
+    console.log(this.username);
+}
+
+chai3();
+
+//2 way of arrow function
+
+//normal way
+const addTwo = (num1, num2) => {
+    return (num1+num2);
+}
+
+//implicite return way part 1
+const addThere = (n1, n2, n3) => n1+n2+n3;
+
+//implicate return way part 2 you can also user () instead of {}
+const addFour = (n1,n2,n3,n4) => (n1+n2+n3+n4);
+
+const student = () => ({name: "ashish", age: "22", skills: "coding"})
+
+
+console.log(addTwo(4,5));
+console.log(addThere(10,2,5));
+console.log(addFour(3,4,5,6));
+console.log(student());
