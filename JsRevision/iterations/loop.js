ffde0ab3cdd29c9bcdf2e7f0a217788017f078df
Ashish Raghuvanshi <31722117+TheTypo36@users.noreply.github.com>
2024-03-20 17:03:45 +0000

    loops:unfinished29

---
+++ JsRevision/iterations/loop.js
@@ -0,0 +1,25 @@
+// ## array specify loops
+
+// for of
+const arr = ["superman","Batman","flash", "wonderwoman"]
+for (const ele of arr) {
+    console.log(ele);
+}
+const greetings = "ashish raghuvanshi";
+
+for (const alph of greetings) {
+    // console.log(alph);
+}
+
+//maps
+const maps = new Map();
+maps.set('IN', "India");
+maps.set('IN', "India");
+maps.set('ENG', "England");
+maps.set('PAK', "Pakistan");
+maps.set('AFG', "Afganistan");
+
+console.log(maps);
+for (const key of maps) {
+    console.log(key);
+}
